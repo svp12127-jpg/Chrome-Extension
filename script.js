0c72f25251a39d352eb4cea0d9b3c90f4049c076
Srivaishnavi-pc <svp12127@gmail.com>
2026-05-21 20:00:04 +0530

    clock

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function clock(){
     const now = new Date()
     let hrs=now.getHours()
-    let mins=now.getMinutes()
+    let mins = String(now.getMinutes()).padStart(2, '0')
     document.getElementById("clock").textContent = `${hrs}:${mins}`
 }
 setInterval(clock, 1000)
